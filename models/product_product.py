14c87808f534ab1ad9491202127c3bc5903b31a8
Cesar250101 <cesar250101@outlook.com>
2023-04-24 12:37:43 -0400

    Generación de SKU automatico

--- models/product_product.py
+++ models/product_product.py
@@ -10,6 +10,32 @@ class ProductProduct(models.Model):
     componente_id = fields.Many2one(comodel_name='method_ltdc.producto_composicion', string='Composición',required=True)
     temporada_id = fields.Many2one(comodel_name='method_ltdc.producto_temporada', string='Temporada',required=True)
 
+    @api.multi
+    def generar_sku(self):
+        if self.seller_ids:
+            for s in self.seller_ids:
+                if s.name and s.product_code:
+                    nombre_completo=s.name.name.split()
+                    nro_elementos=len(nombre_completo)
+                    if nro_elementos==1:
+                        nombre=s.name.name.split()
+                        inicial_nombre=nombre[0][0:2]
+                        sku_generado=inicial_nombre.upper()+'-'+s.product_code                    
+                    else:
+                        nombre,apellido,otros=s.name.name.split()
+                        inicial_nombre=nombre[0]
+                        inicial_apellido=apellido[0]
+                        sku_generado=inicial_nombre.upper()+inicial_apellido.upper()+'-'+s.product_code
+                for a in self.attribute_value_ids:
+                    print(a)
+                    sku_generado+='-'+a.name[0:2]
+            self.default_code=sku_generado
+            self.barcode=sku_generado
+
+    @api.onchange('default_code')
+    def onchange_default_code(self):
+        if self.default_code:
+            self.barcode=self.default_code
 
 class ProductoComposicion(models.Model):
     _name = 'method_ltdc.producto_composicion'
