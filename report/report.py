b3e31e6ac81192bb23f454268d9d83546c061a49
Cesar250101 <Cesar250101@outlook.com>
2023-08-23 14:13:16 -0400

    Reporte de ventas por proveedor

--- report/report.py
+++ report/report.py
@@ -27,31 +27,27 @@ class Ventas(models.Model):
         user=self.env.uid
         tools.drop_view_if_exists(self._cr, self._table)
         self._cr.execute("""
-            CREATE OR REPLACE VIEW %s AS (SELECT ROW_NUMBER() OVER() AS id, ps.name partner_id,pp.id product_id,pt.id product_tmpl_id,pav.id atributo_id,pt.categ_id,
-                mlpc.id composicion_id,mlpt.id  temporada_id,pol.qty cantidad,pol.price_subtotal,po.date_order fecha_orden,
-                'Punto de Venta' origen
-                from pos_order po inner join pos_order_line pol on po.id=pol.order_id 
-                inner join product_product pp on pol.product_id =pp.id 
-                inner join product_template pt on pp.product_tmpl_id =pt.id 
-                inner join product_category pc on pt.categ_id =pc.id 
-                left join product_supplierinfo ps on ps.product_tmpl_id =pt.id 
-                left join product_attribute_value_product_product_rel pavppr on pp.id=pavppr.product_product_id 
-                left join product_attribute_value pav on pavppr.product_attribute_value_id =pav.id
-                left join method_ltdc_producto_composicion mlpc on pt.componente_id =mlpc.id 
-                left join method_ltdc_producto_temporada mlpt on pt.temporada_id =mlpc.id 
-                union 
-                Select ROW_NUMBER() OVER() AS id, ps.name partner_id,pp.id product_id,pt.id product_tmpl_id,pav.id atributo_id,pt.categ_id,
-                mlpc.id composicion_id,mlpt.id  temporada_id,sol.product_uom_qty cantidad,sol.price_subtotal,so.date_order fecha_orden ,
-                'Web' origen
-                from sale_order so inner join sale_order_line sol  on so.id=sol.order_id  
-                inner join product_product pp on sol.product_id =pp.id 
-                inner join product_template pt on pp.product_tmpl_id =pt.id 
-                inner join product_category pc on pt.categ_id =pc.id 
-                left join product_supplierinfo ps on ps.product_tmpl_id =pt.id 
-                left join product_attribute_value_product_product_rel pavppr on pp.id=pavppr.product_product_id 
-                left join product_attribute_value pav on pavppr.product_attribute_value_id =pav.id
-                left join method_ltdc_producto_composicion mlpc on pt.componente_id =mlpc.id 
-                left join method_ltdc_producto_temporada mlpt on pt.temporada_id =mlpc.id 
+            CREATE OR REPLACE VIEW %s AS (SELECT ROW_NUMBER() OVER() AS id, ps.name partner_id,pp.id product_id,pt.id product_tmpl_id,pt.categ_id,
+mlpc.id composicion_id,mlpt.id  temporada_id,pol.qty cantidad,pol.price_subtotal,pol.price_subtotal_incl ,
+po.date_order fecha_orden,'Punto de Venta' origen
+from pos_order po inner join pos_order_line pol on po.id=pol.order_id 
+inner join product_product pp on pol.product_id =pp.id 
+inner join product_template pt on pp.product_tmpl_id =pt.id 
+inner join product_category pc on pt.categ_id =pc.id 
+left join product_supplierinfo ps on ps.product_tmpl_id =pt.id 
+left join method_ltdc_producto_composicion mlpc on pt.componente_id =mlpc.id 
+left join method_ltdc_producto_temporada mlpt on pt.temporada_id =mlpc.id
+union 
+Select ROW_NUMBER() OVER() AS id, ps.name partner_id,pp.id product_id,pt.id product_tmpl_id,pt.categ_id,
+mlpc.id composicion_id,mlpt.id  temporada_id,sol.product_uom_qty cantidad,sol.price_subtotal,sol.price_total ,
+so.date_order fecha_orden ,'Web' origen
+from sale_order so inner join sale_order_line sol  on so.id=sol.order_id  
+inner join product_product pp on sol.product_id =pp.id 
+inner join product_template pt on pp.product_tmpl_id =pt.id 
+inner join product_category pc on pt.categ_id =pc.id 
+left join product_supplierinfo ps on ps.product_tmpl_id =pt.id 
+left join method_ltdc_producto_composicion mlpc on pt.componente_id =mlpc.id 
+left join method_ltdc_producto_temporada mlpt on pt.temporada_id =mlpc.id 
             )
         """ % (
             self._table
