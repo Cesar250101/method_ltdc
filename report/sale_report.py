53de14da9846dbd86df1a5865b639c892132c2d8
Cesar250101 <Cesar250101@outlook.com>
2023-08-25 09:28:05 -0400

    Reporte de venta por proveedores

--- report/sale_report.py
+++ report/sale_report.py
@@ -21,6 +21,7 @@ class Ventas(models.Model):
     price_subtotal_incl = fields.Integer(string='Total')
     fecha_orden = fields.Date(string='Fecha Orden')
     origen = fields.Char(string='Origen')
+    pos = fields.Char(string='Punto de Venta')
 
 
     @api.model_cr
@@ -30,7 +31,7 @@ class Ventas(models.Model):
         self._cr.execute("""
             CREATE OR REPLACE VIEW %s AS (SELECT ROW_NUMBER() OVER() AS id, ps.name partner_id,pp.id product_id,pt.id product_tmpl_id,pt.categ_id,
 mlpc.id composicion_id,mlpt.id  temporada_id,pol.qty cantidad,pol.price_subtotal,pol.price_subtotal_incl ,
-po.date_order fecha_orden,'Punto de Venta' origen
+po.date_order fecha_orden,'Punto de Venta' origen,pc2.name pos
 from pos_order po inner join pos_order_line pol on po.id=pol.order_id 
 inner join product_product pp on pol.product_id =pp.id 
 inner join product_template pt on pp.product_tmpl_id =pt.id 
@@ -38,10 +39,12 @@ inner join product_category pc on pt.categ_id =pc.id
 left join product_supplierinfo ps on ps.product_tmpl_id =pt.id 
 left join method_ltdc_producto_composicion mlpc on pt.componente_id =mlpc.id 
 left join method_ltdc_producto_temporada mlpt on pt.temporada_id =mlpt.id
+inner join pos_session ps2 on po.session_id =ps2.id 
+inner join pos_config pc2 on ps2.config_id =pc2.id 
 union 
 Select ROW_NUMBER() OVER() AS id, ps.name partner_id,pp.id product_id,pt.id product_tmpl_id,pt.categ_id,
 mlpc.id composicion_id,mlpt.id  temporada_id,sol.product_uom_qty cantidad,sol.price_subtotal,sol.price_total ,
-so.date_order fecha_orden ,'Web' origen
+so.date_order fecha_orden ,'Web' origen,'Web' 
 from sale_order so inner join sale_order_line sol  on so.id=sol.order_id  
 inner join product_product pp on sol.product_id =pp.id 
 inner join product_template pt on pp.product_tmpl_id =pt.id 
